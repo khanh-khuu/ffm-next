8e8fa659865b88148a444d44f3a462896de8e477
Khanh Khuu <khuu_giang_khanh@colpal.com>
2025-03-26 21:21:40 +0700

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,16 @@
-import { Box, Button, Card } from "@mantine/core";
-import Home from "@/components/home";
-import Link from "next/link";
-// import { getAvatars } from "./lib/avatar";
+import { Box, Card } from "@mantine/core";
 import FileList from "./file-list";
 import Maker from "./maker";
 
 export default function HomePage() {
-  // const avatars = getAvatars();
-
   return (
     <Box style={{ maxWidth: 900 }} mx="auto" my="lg">
-      {/* <Button>Xin chào</Button> */}
-      {/* <Link href="/avatar">Avatar v1.0.1</Link> */}
-      {/* <Home avatars={avatars} /> */}
       <Card>
-        <Box>
-          <Maker />
-        </Box>
+        <Maker />
       </Card>
 
       <Card mt="xl">
-        <Box>
-          <FileList />
-        </Box>
+        <FileList />
       </Card>
     </Box>
   );
