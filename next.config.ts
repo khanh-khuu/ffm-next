7e6a2f6f2b05225346b147b94d2b8c43264916d0
Khanh Khuu <khuu_giang_khanh@colpal.com>
2025-03-26 19:33:58 +0700

    Add

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 import path from 'path';
+// import CopyPlugin from "copy-webpack-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,9 +8,16 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     config.externals = [...config.externals, 'ffmpeg-ffprobe-static']
+
+    // config.plugins.push(new CopyPlugin({
+    //   patterns: [
+    //     { from: "./node_modules/ffprobe-static/bin", to: "./app/api/upload/bin/" },
+    //   ],
+    // }))
+
     return config;
   },
-  output: "standalone"
+  // output: "standalone"
 };
 
 export default nextConfig;
