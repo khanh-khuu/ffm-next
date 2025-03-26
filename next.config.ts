42d2323d7291749461e9178aa46644169d828e1a
Khanh Khuu <khuu_giang_khanh@colpal.com>
2025-03-26 20:28:25 +0700

    Test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import path from 'path';
-// import CopyPlugin from "copy-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import PermissionsOutputPlugin from 'webpack-permissions-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,11 +10,19 @@ const nextConfig: NextConfig = {
     config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     config.externals = [...config.externals, 'ffmpeg-ffprobe-static']
 
-    // config.plugins.push(new CopyPlugin({
-    //   patterns: [
-    //     { from: "./node_modules/ffprobe-static/bin", to: "./app/api/upload/bin/" },
-    //   ],
-    // }))
+    config.plugins.push(new CopyPlugin({
+      patterns: [
+        { from: "./node_modules/ffmpeg-ffprobe-static/ffmpeg.exe", to: "./app/ffmpeg.exe" },
+        { from: "./node_modules/ffmpeg-ffprobe-static/ffprobe.exe", to: "./app/ffprobe.exe" },
+      ],
+    }))
+    
+    config.plugins.push(new PermissionsOutputPlugin({
+      buildFiles: [
+        "./.next/server/app/ffmpeg.exe",
+        "./.next/server/app/ffprobe.exe",
+      ],
+    }))
 
     return config;
   },
