68f17e36a2d1b66065f11e7e0c6c446b83484e52
Khanh Khuu <khuu_giang_khanh@colpal.com>
2025-03-28 08:36:55 +0700

    Add Active Route

--- src/app/AppShellWrap.tsx
+++ src/app/AppShellWrap.tsx
@@ -4,8 +4,12 @@ import { AppShell, Button, Flex, Group } from "@mantine/core";
 import { IconBrandTelegram, IconEye, IconLiveView, IconWand } from "@tabler/icons-react";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
+import { usePathname } from 'next/navigation'
+
 
 export default function AppShellWrap({ children }: PropsWithChildren) {
+  const pathname = usePathname();
+
   return (
     <AppShell
       header={{
@@ -14,9 +18,9 @@ export default function AppShellWrap({ children }: PropsWithChildren) {
     >
       <AppShell.Header>
         <Group justify="center" align="center" h="60" gap="xs">
-          <Link href={"/"}><Button variant="subtle" leftSection={<IconWand />}>Studio</Button></Link>
-          <Link href={"/media"}><Button variant="subtle" leftSection={<IconBrandTelegram />}>Media</Button></Link>
-          <Link href={"/counter"}><Button variant="subtle" leftSection={<IconEye />}>Counter</Button></Link>
+          <Link href={"/"}><Button variant={pathname === '/' ? 'gradient' : 'transparent'} leftSection={<IconWand />}>Studio</Button></Link>
+          <Link href={"/media"}><Button variant={pathname === '/media' ? 'gradient' : 'transparent'} leftSection={<IconBrandTelegram />}>Media</Button></Link>
+          <Link href={"/counter"}><Button variant={pathname === '/counter' ? 'gradient' : 'transparent'} leftSection={<IconEye />}>Counter</Button></Link>
         </Group>
       </AppShell.Header>
       <AppShell.Main pt="50px">{children}</AppShell.Main>
