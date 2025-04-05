116a2f62a98384a8faa1670c1c331a8753843aa6
Khanh Khuu <khuugiangkhanh@gmail.com>
2025-04-05 12:41:46 +0700

    Tesst

--- src/app/(maker)/_actions/makeVideo2.ts
+++ src/app/(maker)/_actions/makeVideo2.ts
@@ -69,15 +69,15 @@ export default async function makeVideo2(payload: MakeVideoPayload) {
         `[1]scale=${width}*1.5:${height}*1.75[scaled]`,
         '[scaled]rotate=0.4 * (1 + sin(n/100))*PI/180[rotated]',
         `[rotated]crop=${width}:${height}:(in_w-out_w)/2 + 0.35 * (1 + sin(n/100))*(in_w-out_w)/2:(in_h-out_h)/2 + 0.16 * (1 + cos(n/57))*(in_h-out_h)/2[cropped]`,
-        '[cropped]eq=brightness=0.05:contrast=1.03:saturation=1.05[eq]',
-        '[eq]hqdn3d=4.0:3.0:6.0:4.5,unsharp=5:5:1.0:5:5:0.0[output]',
-        ...captions,
-        `[output]pad=iw:ih+ih*0.15:(iw-iw)/2:(ih*0.15)/2:black[output]`,
-        `[output]setpts=PTS/${speed}[video]`,
+        // '[cropped]eq=brightness=0.05:contrast=1.03:saturation=1.05[eq]',
+        // '[eq]hqdn3d=4.0:3.0:6.0:4.5,unsharp=5:5:1.0:5:5:0.0[output]',
+        // ...captions,
+        // `[output]pad=iw:ih+ih*0.15:(iw-iw)/2:(ih*0.15)/2:black[output]`,
+        // `[output]setpts=PTS/${speed}[video]`,
         `[0:a]atempo=${speed}[audio]`,
     ].join(";");
 
-    cmd += `" -map "[video]" -map "[audio]" -shortest output.mp4`;
+    cmd += `" -map "[cropped]" -map "[audio]" -shortest output.mp4`;
 
     if (!process.env.GITHUB_TOKEN) throw new Error("Github Token is missing.");
 
